5d28f4bda0d37fb781443112d692f72ef1bdb8a7
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2022-10-29 12:29:16 -0500

    Web page

--- WebApp/WebApp/views.py
+++ WebApp/WebApp/views.py
@@ -1,19 +1,15 @@
 
 import datetime
+from django.template import Template, Context
 from django.http import HttpResponse
 
 def regards(request): #first view
     return HttpResponse("Hello World Django") 
 
 def dateNow(request):
-    date_now = datetime.datetime.now()
-    document = """
-    <html>
-    <body>
-    <h1>
-    Currently date and time %s
-    </h1>
-    </body>
-    </html>""" % date_now
-
+    doc_externo = open("C:/Users/Alexander/Documents/BYU/BYU-Alex/CSE310/djangoPython/WebApp/WebApp/templates/plantilla.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    document = plt.render(ctx)
     return HttpResponse(document)
